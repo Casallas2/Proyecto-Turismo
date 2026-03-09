f186720d7c165b233fc4315bf9d152fff0e38daf
Alejandro Casallas <jose.benitez.3797@miremington.edu.co>
2026-03-09 11:08:17 -0500

    feat: implementar registro de usuario con NestJS, PostgreSQL y validaciones

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,24 @@
+// src/app.module.ts
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { User } from './users/entities/user.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '123456',
+      database: 'turismo_db',
+      entities: [User],
+      autoLoadEntities: true,
+      synchronize: true,
+      logging: true,
+    }),
+    AuthModule,
+  ],
 })
 export class AppModule {}
